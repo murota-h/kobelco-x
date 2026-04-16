3885d634768ee0ad1553ab4e221d813a4fd024dc
murota-h <renakaren0804@gmail.com>
2026-04-16 15:10:12 +0900

    Fix base-href for GitHub Pages subdirectory

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3363664520" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1826569762" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
